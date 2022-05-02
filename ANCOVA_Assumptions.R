98d4bcfbeccee7be5498c7ade6211932e0920a42
msw5465 <70386500+msw5465@users.noreply.github.com>
2022-05-02 18:39:58 -0400

    Update ANCOVA_Assumptions.R

--- ANCOVA_Assumptions.R
+++ ANCOVA_Assumptions.R
@@ -293,3 +293,26 @@ as.data.frame(
 
 {r caption="Data frame is now printed using `kable`.",render=lemon_print}
 
+# Limitations
+
+With any research project comes any number of limitations and problems that come up throughout the process of the work. We had a few specific limitations that we needed to address and adapt to work out solutions to get around them.
+
+The first problem we encountered was data collection. At first, our group was planning on using the profootballreference.com website to retreive all of our necessary data. However, we quickly discovered that the salary values that were being used were not just rookie contracts. Since we were working with data from 2014 - 2019 NFL seasons, some of the rookie players in those years had already acquired new contracts that were not their rookie value, thus invalidating any results and conclusions we planned to make in relation to rookie contracts. With this issue, we found another data source spotrac.com, where we were able to acquire the rookie information and salary values (in USD) for all NFL rookies in the 2019 - 2021 NFL seasons. Thus, with a simple change in years being studied and a new data source, we were able to overcome this limitation.
+
+The second problem we encountered was the use of the parametric shortcut for ANOVA. Our original plan was to try a parametric shortcut ANOVA model after checking our assumptions, but once we had created our plots for gaussian residuals assumptions and homoscadesticity, we quickly realized that these two assumptions failed very badly. We then tried to add transformations which only appeared to make the plots worse. Thus, this left us to continue with the nonparametric shortcut.
+  
+After further considerations, we realized that the nonparametric shortcut also would not work due to our lack of independence of observations, since draft pick order does affect salary value. Thus, we had to make one final switch to include draft pick order into our model, leaving our group with the ANCOVA method as our choice of study. 
+
+Even though we encountered very important limitations through our research, we were able to adapt quickly and make-up the work we needed to complete in order to produce a proper ANCOVA analysis for NFL Rookie Salary Values.
+
+\newpage
+
+# Author Contributions
+
+The authors of this report would like to acknowledge their individual contributions to the report.
+
++ Mitchell Wentzel contributed to the design of the study, collection of data, analysis of data, and writing of the report.
++ Mason Conrad contributed to the design of the study, collection of data, analysis of data, and writing of the report.
++ Luke Geier contributed to the design of the study, collection of data, analysis of data, and writing of the report.
++ Marquis Gee contributed to the design of the study, collection of data, analysis of data, and writing of the report.
+
